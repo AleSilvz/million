651aec067cc9b0785cfb1320e7828f4f25785ae7
AleSilvz <realmadrid201589@gmail.com>
2026-01-05 01:42:26 -0300

    ONE MILLION

---
+++ src/pages/goals.jsx
@@ -0,0 +1,64 @@
+import { useLocation } from "react-router";
+import "./Goals.css";
+import { useEffect, useState } from "react";
+
+export default function Goals() {
+  const goal = useLocation().state?.goal;
+  const local = JSON.parse(localStorage.getItem("goalsData")) || {};
+  const [balance, setBalance] = useState(0);
+
+  function updateBalance() {
+    setBalance((local[goal] || []).reduce((acc, e) => acc + Number(e), 0));
+  }
+
+  useEffect(() => {
+    updateBalance();
+  }, []);
+
+  function addValue(e) {
+    const valueDeposite = Number(e.currentTarget.dataset.value);
+
+    if (!local[goal]) {
+      local[goal] = [];
+    }
+
+    const set = new Set(local[goal] || []);
+
+    set.has(valueDeposite) ? set.delete(valueDeposite) : set.add(valueDeposite);
+
+    local[goal] = [...set];
+
+    localStorage.setItem("goalsData", JSON.stringify(local));
+    updateBalance();
+  }
+
+  return (
+    <div className="container-g">
+      <h1>
+        Goals {goal} - current balance{" "}
+        {balance.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </h1>
+
+      <div className="content-g">
+        {Array.from({ length: goal }, (_, i) => {
+          const value = i + 1;
+          const select = local[goal] || [];
+
+          return (
+            <div
+              key={i}
+              className={select.includes(value) ? "goal-c" : "goal-g"}
+              data-value={value}
+              onClick={(e) => addValue(e)}
+            >
+              {select.includes(value) ? "✔" : value}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,57 @@
+import { Router, useNavigate } from "react-router";
+import "./Home.css";
+import { useEffect, useState } from "react";
+export default function Home() {
+  const navigate = useNavigate();
+  const local = JSON.parse(localStorage.getItem("goalsData"));
+  const [currentBalance, setCurrentBalance] = useState(0);
+
+  function renderCurrentBalance() {
+    const balance = Object.values(local).reduce((acc, valor) => {
+      return acc + valor.reduce((acc, i) => acc + i, 0);
+    }, 0);
+
+    setCurrentBalance(balance);
+  }
+
+  useEffect(() => {
+    renderCurrentBalance();
+  }, []);
+
+  function goToGoal(e) {
+    navigate(`/goals`, {
+      state: {
+        goal: e.currentTarget.dataset.value,
+      },
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>One Million</h1>
+
+      <h1>Goals</h1>
+
+      <div className="goals">
+        {Array.from({ length: 11 }, (_, i) => (
+          <div
+            key={i}
+            className="goal"
+            data-value={100 + i * 50}
+            onClick={(e) => goToGoal(e)}
+          >
+            {100 + i * 50}
+          </div>
+        ))}
+      </div>
+
+      <h1>current balance</h1>
+      <h1>
+        {currentBalance.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </h1>
+    </div>
+  );
+}
